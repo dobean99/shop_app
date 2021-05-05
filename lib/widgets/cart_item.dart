6e5632da9682e0330d07a9f531a72426d81cf08a
= <56818006+DoNgocKimDo@users.noreply.github.com>
2021-05-05 15:44:05 +0700

    Showing alert dialogs delete item in Cart.

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -9,7 +9,7 @@ class CartItem extends StatelessWidget {
   final double price;
   final int quantity;
 
-  CartItem(this.id, this.productId,this.title, this.price, this.quantity);
+  CartItem(this.id, this.productId, this.title, this.price, this.quantity);
 
   @override
   Widget build(BuildContext context) {
@@ -22,14 +22,34 @@ class CartItem extends StatelessWidget {
           color: Colors.white,
           size: 40,
         ),
-        alignment: Alignment.center,
-        padding: EdgeInsets.only(right: 20),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 10),
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
       direction: DismissDirection.endToStart,
-      onDismissed: (direction)
-      {
-        Provider.of<Cart>(context,listen: false).removeItem(productId);
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to delete this item?'),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop(true);
+                  },
+                  child: Text('Yes')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop(false);
+                  },
+                  child: Text('No')),
+            ],
+          ),
+        );
+      },
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
       child: Card(
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
